2ad4d7739fd23c90878a21e98c3449e1a1638268
SkyGamer24 <59799612+SkyGamer24@users.noreply.github.com>
2020-07-24 17:15:43 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -647,10 +647,10 @@ async def on_member_join(member):
     joinembed = discord.Embed(description = f'{member.mention} ist dem Server beigetreten.', colour = farbton4)
     await logging.send(embed=joinembed)
     
-    welcome = discord.Embed(title=''':flag_de: __German__:
+    welcome = discord.Embed(title=''':flag_de: *__German:__*
 
 Herzlich Willkommen,''', description = '''
-    am offiziellen Discord-Server von __Z-Software__ und __Aerosoft__ rund um den __Autobahn-Polizei Simulator 3__.
+    am offiziellen Discord-Server von __Z-Software__ und __Aerosoft__ rund um den **__Autobahn-Polizei Simulator 3__**.
     Bitte mach dich zum Beginn mit unseren Regeln (<#681876950809444371>) vertraut.
     Außerdem solltest du einen Blick in (<#723611371660378173>) werfen.
     Dort findest du alle interessanten Informationen, Fragen und Antworten, sowie wissenswerte Details rund um den 3. Teil der Polizei-Simulation.
@@ -661,10 +661,10 @@ Herzlich Willkommen,''', description = '''
     Dein Entwicklungs-Team
 
 ''', colour = 0xFF0000 )
-    englischembed = discord.Embed(title=''':flag_gb: __English__:
+    englischembed = discord.Embed(title=''':flag_gb: *__English:__*
 
 Welcome,''', description='''
-    at the official discord server of __Z-Software__ and __Aerosoft__ around the __Autobahn-Police Simulator 3__.
+    at the official discord server of __Z-Software__ and __Aerosoft__ around the **__Autobahn-Police Simulator 3__**.
     Please make yourself familiar with our rules (<#681876950809444371>).
     You should also have a look at (<#723611371660378173>).
     There you will find all interesting information, questions and answers, as well as interesting details about the 3rd part of the police simulation.
